8f97b1149328568aa710399e5233d3798231b8f4
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-08-18 16:16:58 +0900

    Issue4-6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,8 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-
-  def getWeek
+  def get_week
     @wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
 
@@ -36,9 +35,7 @@ class CalendarsController < ApplicationController
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => plans, wdays: @wdays[x]}
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: plans, wdays: @wdays[x]}
       @week_days.push(days)
     end
   end
